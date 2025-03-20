c8b39651d61fffc5fc892012d63e2b6461f5338a
Sandeep-Narsi-AI <93811560+Sandeep-Narsi-AI@users.noreply.github.com>
2025-03-20 17:10:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
+import subprocess
 
+# Try installing langchain-openai
+subprocess.run(["pip", "install", "langchain-openai"], check=True)
+
+import os
+import pickle
+import streamlit as st
+import time
+from langchain_openai import ChatOpenAI 
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from dotenv import load_dotenv
 
